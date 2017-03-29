8c8ba13c7306995bf06faa1f4b7a7c863cc2e1fd
sam <samyankun@gmail.com>
2017-03-30 00:14:14 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-apt-get install libsdl-1.2-dev flex bison
+#apt-get install libsdl-1.2-dev flex bison
+apt-get install  flex bison
 git submodule update --init pixman
 git submodule update --init dtc
+./configure --enable-kvm --enable-tpm  --enable-sdl
 make -j4
 make install
 
